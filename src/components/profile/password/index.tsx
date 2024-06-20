f7c2817585c2b942f3c24f7dd6737fb5fd9cff94
brunopilotoa <52364626+brunopilotoa@users.noreply.github.com>
2024-06-20 12:09:42 -0300

    criado estrutura de senha atualizado visual da parte do password

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -4,63 +4,58 @@ import styles from"../../../../styles/profile.module.scss";
 const PasswordForm = function (){
     
     return(
-    <>
-    <Form className={styles.form}>
-    <div className={styles.inputNormalDiv}>
-        <FormGroup>
-            <Label for="currentPassword" className={styles.label}>SENHA ATUAL</Label>
-            <Input
-            name="currentPassword"
-            type="password"
-            id="currentPassword"
-            placeholder="*******"
-            required
-            minLength={6}
-            maxLength={12}
-            className={styles.input}
-            ></Input>
-        </FormGroup>
+  <>
+	<Form className={styles.form}>
+	  <div className={styles.inputNormalDiv}>
+	    <FormGroup>
+	      <Label className={styles.label} for="currentPassword">
+	        SENHA ATUAL
+        </Label>
+        <Input
+        name="currentPassword"
+        type="password"
+        id="currentPassword"
+        placeholder="******"
+        required
+        maxLength={12}
+        className={styles.input}
+        />
+      </FormGroup>
     </div>
     <div className={styles.inputFlexDiv}>
-        <FormGroup>
-            <Label for="newPassword" className={styles.label}>
-            NOVA SENHA
-            </Label>
-            <input 
-            name="newPassword"
-            type="password" 
-            id="newPassword"
-            placeholder="*******"
-            required
-            minLength={6}
-            maxLength={12}
-            className={styles.inputFlex}
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="confirmNewPassword" className={styles.label}>
-            CONFIRMAR NOVA SENHA
-            </Label>
-            <input 
-            name="confirmNewPassword"
-            type="password" 
-            id="confirmNewPassword"
-            placeholder="*******"
-            required
-            minLength={6}
-            maxLength={12}
-            className={styles.inputFlex}
-            />
-        </FormGroup>
-        </div>
-        <Button
-        className={styles.formBtn}
-        outline
-        >
-            Salvar Alterações
-        </Button>
-    </Form>
-    </>
+	    <FormGroup>
+	      <Label className={styles.label} for="newPassword">
+	        NOVA SENHA
+        </Label>
+        <Input
+        name="newPassword"
+        type="password"
+        id="newPassword"
+        placeholder="******"
+        required
+        className={styles.inputFlex}
+        />
+      </FormGroup>
+	    <FormGroup>
+	      <Label className={styles.label} for="confirmNewPassword">
+		      CONFIRMAR NOVA SENHA
+	      </Label>
+	      <Input
+		    name="confirmNewPassword"
+		    type="password"
+	      id="confirmNewPassword"
+	      placeholder="******"
+	      required
+	      className={styles.inputFlex}
+	      />
+	    </FormGroup>
+
+      <Button className={styles.formBtn} outline>
+	      Salvar Alterações
+      </Button>
+    </div>
+  </Form>
+</>
     )
 }
 
